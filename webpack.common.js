23edd6aa1fe51e763442b71609363d056255c2c6
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-04-26 13:11:05 +0800

    modify webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/template.html'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Production',
+      template: './src/template.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
